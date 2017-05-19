0220a5bb98f9dcf7f5d647446abf5238a48c2bc0
Danielle YoungSmith <dyoungsmith@gmail.com>
2017-05-18 19:47:47 -0700

    converts budget in millions to numbers

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ rp('http://oscars.yipitdata.com/')
             let currFilm = yr.films[i];
 
             if (currFilm.Winner) {
-                detailPromises.push(rp(currFilm['Detail URL']));    // Build array for Promise.all
+                detailPromises.push(rp(currFilm['Detail URL']));    // Build details array for Promise.all
                 winners.push({
                     year: yr.year,
                     title: currFilm.Film
@@ -38,16 +38,50 @@ rp('http://oscars.yipitdata.com/')
 .then(({ budgets, winners }) => {
     // Condense the winner information
     winners.forEach((winner, i) => {
-        // Clean up year string
+        // YEAR: Clean up year string
+        const year = winner.year;
         const yrRE = /^\d{4}/;  // one year: '2010'
         const yrRangeRE = /^\d{4}\s\/\s\d{2}/;  // year range: '1927 / 28'
-        let yr = winner.year.match(yrRangeRE) || winner.year.match(yrRE);
-        winner.year = yr[0];
+        const yrMatches = year.match(yrRangeRE) || year.match(yrRE);
+        winner.year = yrMatches[0];
 
-        // Transform budget and add to winner
+        // BUDGET: Transform budget and add to winner
         // str >> Number; GBP >> USD
-        winner.budget = budgets[i];
+        let budgCount = 0,
+            totalBudg = 0;
+        const budg = budgets[i];
+        const decimal = /\d+\.\d+/;    // '1.2', '16.5' (ranges limited to min)
+        const mil = /\d+,\d+,\d+/;  // '3,456,432'
+        const thou = /\d+,\d+/;  // '3,456'
+        const short = /\d+/;   // '15 million' matches '15'
+        const million = /million/i;
+        const usd = /us/i;
+
+        if (budg) {
+            let numericalBudg;
+            budgCount++;
+            // grab numerical portion
+            // if (budg.match(decimal)) {
+            //     numericalBudg = Number(budg.match(decimal)[0].substring(1));
+            //     if (budg.match(million)) numericalBudg *= 1000000;
+
+            // }
+            if (budg.match(mil) || budg.match(thou)) {
+                numericalBudg = budg;
+            }
+            else if (budg.match(decimal) || budg.match(short)) {
+                const budgMatch = budg.match(decimal) || budg.match(short);
+                numericalBudg = Number(budgMatch[0]);
+                if (budg.match(million)) numericalBudg *= 1000000;
+                // Convert GBP >> USD
+            }
+            totalBudg += numericalBudg;
+            winner.budget = numericalBudg;
+        }
+        else { winner.budget = budg; }
+        const avgBudg = totalBudg / budgCount;
     });
+
     console.log('WINNERS: ', winners);
 })
 .catch(err => console.error(err));
