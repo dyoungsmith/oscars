0c642cee12d274fb89bbb82b9ab5c67bc1e448cb
Danielle YoungSmith <dyoungsmith@gmail.com>
2017-05-18 17:31:49 -0700

    gets all relevant info

--- index.js
+++ index.js
@@ -13,22 +13,25 @@ rp('http://oscars.yipitdata.com/')
         for (let i = 0; i < yr.films.length; i++) {
             let currFilm = yr.films[i];
             if (currFilm.Winner) {
-                // detailPromises.push(axios.get(currFilm['Detail URL']));
-                axios.get(currFilm['Detail URL'])
-                .then(res => JSON.parse(res.data).Budget)
+                rp(currFilm['Detail URL'])
+                .then(res => JSON.parse(res).Budget)    // EDGE: some budgets undefined
                 .then(budget => {
-                    winners.push({
-                        year: yr.year,
-                        title: currFilm.Film,
-                        budget
-                    });
+                    // console.log('BUDGET', budget);
+                    if (budget) {
+                        winners.push({
+                            year: yr.year,
+                            title: currFilm.Film,
+                            budget
+                        });
+                    }
+                    // console.log('WINNERS: ', winners);
                 })
                 .catch(err => console.error(err));
                 break;
             }
         }
-        console.log('WINNERS: ', winners);
     });
+    return winners;
 
     // let detailProms = Promise.all(detailPromises).then(res;
 
@@ -41,55 +44,13 @@ rp('http://oscars.yipitdata.com/')
     //     return { details: deets, winners }
     // });
 })
+.then(winners => {
+    console.log('WINNERS: ', winners);
+})
 // .then(({ details, winners }) => {
 //     // console.log('DEETS: ', details[0].results);
 //     // console.log('DEETS: ', details, 'WINNERS: ', winners);
 // })
 .catch(err => console.error(err));
-
-// request('http://oscars.yipitdata.com/', (err, res) => {
-//     let winners = [];
-//     if (err) console.error(err);
-//     let nomsByYr = JSON.parse(res.body).results;  // All nominees by year
-
-//     let currFilm = nomsByYr[0].films[0];    // TESTING
-
-//     if (currFilm.Winner) {
-//         let detailsURI = currFilm['Detail URL'];
-
-//         return request(detailsURI, (error, response) => {
-//             if (err) console.error(error);
-//             let filmDetails = JSON.parse(response.body);
-//             let winnerInfo = {
-//                 year: nomsByYr[0].year,
-//                 title: currFilm['Film'],
-//                 budget: filmDetails['Budget']
-//             }
-//             winners.push(winnerInfo);
-//             // console.log('WINNER INFO: ', winnerInfo);
-//             console.log('WINNER: ', winners);
-//         })
-//     }
-
-    // nomsByYr.forEach(yr => {
-    //     for (let i = 0; i < yr.films.length; i++) {
-    //         let currFilm = yr.films[i];
-    //         if (currFilm.Winner) {
-    //             request(currFilm['Detail URL'], (error, response) => {
-    //                 if (error) console.error(error);
-    //                 let filmDetails = JSON.parse(response.body);
-    //                 winners.push({
-    //                     year: yr.year,
-    //                     title: currFilm['Film'],
-    //                     budget: filmDetails['Budget']
-    //                 });
-    //                 console.log('WINNER ARR: ', winners);
-    //             });
-    //             break;
-    //         }
-    //     }
-    // });
-    // console.log('WINNERS: ', winners);
-// });
 
 
